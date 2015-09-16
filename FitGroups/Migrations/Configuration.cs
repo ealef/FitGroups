ded0e8773a527b95262d77a59ff69edf12be2ee6
ealef <eugene.alefragi@gmail.com>
2015-09-16 10:46:06 +0300

    Working with IDENTITY
    The project works with ASP.NET Identity authorization, for Mysql

---
+++ FitGroups/Migrations/Configuration.cs
@@ -0,0 +1,22 @@
+namespace FitGroups.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FitGroups.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+
+            // register mysql code generator
+            SetSqlGenerator("MySql.Data.MySqlClient", new MySql.Data.Entity.MySqlMigrationSqlGenerator());
+
+            SetHistoryContextFactory("MySql.Data.MySqlClient", (conn, schema) => new MySqlHistoryContext(conn, schema));
+        }
+
+        protected override void Seed(FitGroups.Models.ApplicationDbContext context)
+        {
+
+        }
+    }
+}
---
+++ FitGroups/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitGroups.Startup))]
+namespace FitGroups
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
